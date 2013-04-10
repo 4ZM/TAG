5b475603610c9b31cf8f1e51885dad8e30a23e93
Anders Sundman <anders@4zm.org>
2013-04-10 17:17:15 +0200

    Added boiler plate NFC code

--- src/org/sparvnastet/tag/TagActivity.java
+++ src/org/sparvnastet/tag/TagActivity.java
@@ -19,16 +19,48 @@
 
 package org.sparvnastet.tag;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.Menu;
 
 public class TagActivity extends Activity {
 
+    private NfcAdapter mAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mFilters;
+    private String[][] mTechLists;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tag_layout);
+
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        // Setup foreground processing of NFC intents
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter techFilter = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        mFilters = new IntentFilter[] { techFilter };
+        mTechLists = new String[][] { new String[] { MifareClassic.class.getName() } };
+
+        Intent intent = getIntent();
+        resolveIntent(intent);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.i("TAG", "NFC: onNewIntent");
+        setIntent(intent);
+        resolveIntent(intent);
     }
 
     @Override
@@ -38,4 +70,54 @@ public class TagActivity extends Activity {
         return true;
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i("TAG", "NFC: disableForegroundDispatch");
+        if (mAdapter != null)
+            mAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("TAG", "NFC: enableForegroundDispatch");
+        if (mAdapter != null)
+            mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
+    }
+
+    void resolveIntent(Intent intent) {
+        Log.i("TAG", "resolveIntent action=" + intent.getAction());
+
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+
+            Parcelable tags = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if (tags == null) {
+                Log.i("TAG", "resolveIntent: ParcelableExtra (tag) was null");
+                return;
+            }
+
+            tagDetected((Tag) tags);
+        }
+    }
+
+    private void tagDetected(Tag tag) {
+        MifareClassic mifareTag = MifareClassic.get(tag);
+        if (mifareTag == null) {
+            Log.i("TAG", "Unknown tag type found (not MifareClassic)");
+            return;
+        }
+
+        readTag(mifareTag);
+        writeTag(mifareTag);
+    }
+
+    public void readTag(MifareClassic tag) {
+
+    }
+
+    public void writeTag(MifareClassic tag) {
+
+    }
 }
